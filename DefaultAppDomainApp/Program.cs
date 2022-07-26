0e28ae2390c902622a1ab2ad2cd1ddecde703602
Sergey Misko <spy_3462@mail.ru>
2022-07-26 17:52:00 +0400

    исправил ошибку

--- DefaultAppDomainApp/Program.cs
+++ DefaultAppDomainApp/Program.cs
@@ -15,7 +15,7 @@ namespace DefaultAppDomainApp
             AppDomain defaultAD = AppDomain.CurrentDomain;
             // Вывести разнообразные статистические данные об этом домене.
             Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
-            Console.WriteLine("ID of domain in this process: {)}", defaultAD.Id);
+            Console.WriteLine("ID of domain in this process: {0}", defaultAD.Id);
             Console.WriteLine("Is this the default domain?: {0}",defaultAD.IsDefaultAppDomain());
             Console.WriteLine("Base directory of this domain: {0}",defaultAD.BaseDirectory);
         }
