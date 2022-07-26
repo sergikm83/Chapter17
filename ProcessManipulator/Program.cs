34f2ebd7298c00dfd53c8e113918d7542adc048f
Sergey Misko <spy_3462@mail.ru>
2022-07-26 12:45:42 +0400

    добавил метод ListAllRunningProcesses() для получения и вывода всех запущенных процессов на текущей машине, сгруппированных по PID

--- ProcessManipulator/Program.cs
+++ ProcessManipulator/Program.cs
@@ -10,5 +10,19 @@ namespace ProcessManipulator
         {
             Console.WriteLine("Hello World!");
         }
+        static void ListAllRunningProcesses()
+        {
+            // Получить все процессы на локальной машине, упорядоченные по PID.
+            var runningProcs = from proc in Process.GetProcesses(".")
+                               orderby proc.Id
+                               select proc;
+            // Вывести для каждого процесса идентификатор PID и имя.
+            foreach(var p in runningProcs)
+            {
+                string info = $"-> PID: {p.Id}\tName: {p.ProcessName}";
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("***************************************"\n);
+        }
     }
 }
