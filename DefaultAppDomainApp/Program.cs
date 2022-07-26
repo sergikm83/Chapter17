292d154d503f66b99eab6c4697bd014f35eae274
Sergey Misko <spy_3462@mail.ru>
2022-07-26 17:50:32 +0400

    добавил метод DisplayDADStats(), который выводит детальные сведения о стандартном домене приложения

--- DefaultAppDomainApp/Program.cs
+++ DefaultAppDomainApp/Program.cs
@@ -8,5 +8,15 @@ namespace DefaultAppDomainApp
         {
             Console.WriteLine("Hello World!");
         }
+        private static void DisplayDADStats()
+        {
+            // Получить доступ к домену приложения для текущего потока.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            // Вывести разнообразные статистические данные об этом домене.
+            Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
+            Console.WriteLine("ID of domain in this process: {)}", defaultAD.Id);
+            Console.WriteLine("Is this the default domain?: {0}",defaultAD.IsDefaultAppDomain());
+            Console.WriteLine("Base directory of this domain: {0}",defaultAD.BaseDirectory);
+        }
     }
 }
