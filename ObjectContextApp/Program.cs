cd103ed513cf32785659b811a4dfc95520bc2fb7
Sergey Misko <spy_3462@mail.ru>
2022-07-27 17:42:30 +0400

    добавил проект ObjectContextApp

---
+++ ObjectContextApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectContextApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
